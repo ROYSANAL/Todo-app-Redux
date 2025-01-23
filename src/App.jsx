3a74f3fe7f4b103ffc11e67047f873b7c48ea863
ROYSANAL <sanalroy12345@gmail.com>
2025-01-24 02:26:01 +0530

    added updateTodo action

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import AddTodo from './components/AddTodo'
-import Todos from './components/Todos'
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
-  
   return (
     <>
       <AddTodo />
       <Todos />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
